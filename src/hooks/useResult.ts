df47ee0c4e02b057b7ccf85720ed915a507fbd69
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-09-10 22:02:08 +0300

    fix(useResult): change to useEffect-based lifecycle
    requires some ugly hacking around the rough edges
    to make sure we start on-mount and not cause a redundant render
    blegh

--- src/hooks/useResult.ts
+++ src/hooks/useResult.ts
@@ -17,16 +17,11 @@ export const makeUseResult: <R>(
 ) => UseResult<R> = <R>(runtimeContext: RuntimeContext<R>) =>
   <R0 extends R, E, A>(stream: Stream.Stream<R0, E, A>) => {
     const runtime = useContext(runtimeContext)
-    const prevRef = useRef<Stream.Stream<R0, E, A>>()
     const fiberRef = useRef<Fiber.RuntimeFiber<E, void>>()
+    const firstTimeRef = useRef(true)
     const [result, setResult] = useState<Result.Result<E, A>>(Result.waiting(Result.initial()))
     const [trackRef, resultBag] = useResultBag(result)
-
-    if (prevRef.current !== stream) {
-      prevRef.current = stream
-      if (fiberRef.current) {
-        Effect.runSync(Fiber.interruptFork(fiberRef.current))
-      }
+    if (!fiberRef.current) {
       fiberRef.current = stream.pipe(
         Stream.tap((value) =>
           Effect.sync(() => {
@@ -43,14 +38,31 @@ export const makeUseResult: <R>(
       )
     }
 
-    trackRef.current.currentStatus = result._tag
-
-    useEffect(() =>
-      () => {
+    useEffect(() => {
+      if (!firstTimeRef.current) {
+        fiberRef.current = stream.pipe(
+          Stream.tap((value) =>
+            Effect.sync(() => {
+              setResult(updateNext(Result.success(value), trackRef))
+            })
+          ),
+          Stream.tapErrorCause((cause) =>
+            Effect.sync(() => {
+              setResult(updateNext(Result.failCause(cause), trackRef))
+            })
+          ),
+          Stream.runDrain,
+          Runtime.runFork(runtime)
+        )
+      }
+      firstTimeRef.current = false
+      return () => {
         if (fiberRef.current) {
           Effect.runSync(Fiber.interruptFork(fiberRef.current))
         }
-      }, [])
+      }
+    }, [runtime, stream])
 
+    trackRef.current.currentStatus = result._tag
     return resultBag
   }
