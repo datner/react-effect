e8f2cd054c6c463624f6a248a783ab1bc8ee5808
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-09-17 19:04:41 +0300

    test: refine tests
    also remove some patching around behavior
    
    Signed-off-by: Yuval Datner <22598347+datner@users.noreply.github.com>

--- test/hooks/useResultCallback.ts
+++ test/hooks/useResultCallback.ts
@@ -17,20 +17,48 @@ describe("useResultCallback", () => {
   it("should do good", async () => {
     const testEffect = (value: number) => Effect.succeed(value)
 
-    const { result } = await waitFor(async () => renderHook(() => useResultCallback(testEffect)))
+    const { result } = renderHook(() => useResultCallback(testEffect))
 
-    expect(Result.isInitial(result.current[0].result)).toBe(true)
+    assert.isTrue(Result.isInitial(result.current[0].result))
     act(() => {
       result.current[1](1)
     })
-    await waitFor(() => expect(Result.isSuccess(result.current[0].result)).toBe(true))
-    assert(Result.isSuccess(result.current[0].result))
-    expect(result.current[0].result.value).toBe(1)
+    await waitFor(() => {
+      assert(Result.isSuccess(result.current[0].result))
+      return expect(result.current[0].result.value).toBe(1)
+    })
     act(() => {
       result.current[1](2)
     })
-    await waitFor(() => expect(Result.isSuccess(result.current[0].result)).toBe(true))
-    assert(Result.isSuccess(result.current[0].result))
-    expect(result.current[0].result.value).toBe(2)
+    await waitFor(() => {
+      assert(Result.isSuccess(result.current[0].result))
+      return expect(result.current[0].result.value).toBe(2)
+    })
+  })
+
+  it("should do good async", async () => {
+    const testEffect = (value: number) =>
+      Effect.async<never, never, number>((cb) => {
+        setTimeout(() => cb(Effect.succeed(value)), 100)
+      })
+    const { result } = renderHook(() => useResultCallback(testEffect))
+
+    assert.isTrue(Result.isInitial(result.current[0].result))
+    act(() => {
+      result.current[1](1)
+    })
+    await waitFor(() => assert.isTrue(result.current[0].isLoading))
+    await waitFor(() => {
+      assert(Result.isSuccess(result.current[0].result))
+      return expect(result.current[0].result.value).toBe(1)
+    })
+    act(() => {
+      result.current[1](2)
+    })
+    await waitFor(() => assert.isTrue(result.current[0].isRefreshing))
+    await waitFor(() => {
+      assert(Result.isSuccess(result.current[0].result))
+      return expect(result.current[0].result.value).toBe(2)
+    })
   })
 })
